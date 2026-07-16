b3a3cd94ffbf8f8922c5fd8f08018c9521217b1a
A <170555190+AaishQ@users.noreply.github.com>
2026-07-16 14:39:29 +0100

    ParallelContextHolder.java

---
+++ Implementation/ParallelContextHolder.java
@@ -0,0 +1,13 @@
+
+// This class holds a single instance of a parallel context
+public class ParallelContextHolder {
+    private static ParallelContext context = null;
+
+    public static void set( ParallelContext context_ ) {
+	context = context_;
+    }
+
+    public static ParallelContext get() {
+	return context;
+    }
+}
